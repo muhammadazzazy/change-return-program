337adba33eeffc3ba813c95a101d4e7473d73430
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-15 12:48:11 +0200

    added exception handling and type annotations

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
+from sys import exit
+
+
 def main() -> None:
+    exit_message: str = 'Exiting program...'
     while True:
         try:
-            user_input = input('Enter the cost: ')
+            user_input: str = input('Enter the cost: ')
             if user_input == 'exit':
-                break
-            cost = float(user_input)
+                print(exit_message)
+                exit()
+            if user_input[0] == '$':
+                cost: float = float(user_input[1:])
+            else:
+                cost: float = float(user_input)
+
             user_input = input('Enter the amount of money given: ')
-            money_given = float(user_input)
+            if user_input[0] == '$':
+                money_given: float = float(user_input[1:])
+            else:
+                money_given: float = float(user_input)
+        except IndexError:
+            print('Please enter some valid input...')
+            continue
         except ValueError:
-            print('Enter a floating-point value!')
+            print('Please enter some valid input...')
             continue
+        except KeyboardInterrupt:
+            print(exit_message)
+            exit()
 
-        change = round(money_given - cost, 2)
+        change: float = round(money_given - cost, 2)
         if change < 0:
-            print('Cost should be at most equal to amount of money given!')
+            print('Cost should be at most equal to amount of money given...')
             continue
 
-        diff = change
-        num_of_quarters = 0
-        num_of_dimes = 0
-        num_of_nickels = 0
-        num_of_pennies = 0
+        diff: float = change
+        num_of_quarters: int = 0
+        num_of_dimes: int = 0
+        num_of_nickels: int = 0
+        num_of_pennies: int = 0
+
         while diff > 0:
             if (diff-0.25) >= 0:
                 diff = round(diff - 0.25, 2)
@@ -34,7 +53,9 @@ def main() -> None:
             elif (diff-0.01) >= 0:
                 diff = round(diff - 0.01, 2)
                 num_of_pennies += 1
+
         print(f'The change is ${change:.2f}.')
+
         print(f'{num_of_quarters} quarters, {num_of_dimes} dimes, {num_of_nickels} nickels, and {num_of_pennies} pennies are required for this change.')
 
 
